708322db4ba1e817ac885e94a0f3928b9c1081f3
happydagui <happyagui@126.com>
2016-12-13 21:04:22 +0800

    initial commit

--- react/demo/src/App.js
+++ react/demo/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Foo from './Foo';
 
 class App extends Component {
   render() {
@@ -14,7 +13,6 @@ class App extends Component {
         <p classname="app-intro">
           to get started, edit <code>src/app.js</code> and save to reload.
         </p>
-        <Foo />
       </div>
     );
   }
